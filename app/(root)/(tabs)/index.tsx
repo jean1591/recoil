f249936ab06d9265faa58df3600f054055861c30
Jean Robertou <j-rob@live.fr>
2025-02-16 11:59:04 +0100

    feat: home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,150 +1,88 @@
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { Card, FeaturedCard } from "@/components/Cards";
-import { getLatestProperties, getProperties } from "@/lib/appwrite";
-import { router, useLocalSearchParams } from "expo-router";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 
-import Filters from "@/components/Filters";
-import NoResults from "@/components/NoResults";
+import { FeaturedCard } from "@/components/Cards";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Search from "@/components/Search";
-import icons from "@/constants/icons";
+import { getLatestProperties } from "@/lib/appwrite";
+import { router } from "expo-router";
 import { useAppwrite } from "@/lib/useAppWrite";
-import { useEffect } from "react";
 import { useGlobalContext } from "@/lib/globalProvider";
 
 export default function Index() {
   const { user } = useGlobalContext();
-  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
 
   const { data: latestProperties, loading: latestPropertiesLoading } =
     useAppwrite({
       fn: getLatestProperties,
     });
 
-  const {
-    data: properties,
-    loading,
-    refetch,
-  } = useAppwrite({
-    fn: getProperties,
-    params: {
-      filter: params.filter!,
-      query: params.query!,
-      limit: 6,
-    },
-    skip: true,
-  });
-
-  useEffect(() => {
-    refetch({
-      filter: params.filter!,
-      query: params.query!,
-      limit: 6,
-    });
-  }, [params.filter, params.query]);
-
   const handleCardPress = (id: string) => {
     return router.push(`/properties/${id}`);
   };
 
   return (
-    <SafeAreaView className="bg-white h-full">
-      <FlatList
-        keyExtractor={(item) => item.$id}
-        ListEmptyComponent={
-          loading ? (
-            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
-          ) : (
-            <NoResults />
-          )
-        }
-        numColumns={2}
-        contentContainerClassName="pb-32"
-        columnWrapperClassName="flex gap-5 px-5"
-        showsVerticalScrollIndicator={false}
-        data={properties}
-        renderItem={({ item }) => (
-          <Card item={item} onPress={() => handleCardPress(item.$id)} />
-        )}
-        ListHeaderComponent={
-          <View className="px-5">
-            <View className="flex flex-row items-center justify-between mt-5">
-              <View className="flex flex-row">
-                <Image
-                  source={{ uri: user?.avatar }}
-                  className="size-12 rounded-full"
-                />
-                <View className="flex flex-col items-start ml-2 justify-center">
-                  <Text className="text-xs font-rubik text-black-100">
-                    Good morning !
-                  </Text>
-                  <Text className="text-base font-rubik-medium text-black-300">
-                    {user?.name}
-                  </Text>
-                </View>
-              </View>
-
-              <Image source={icons.bell} className="size-6" />
-            </View>
-
-            <Search />
-
-            <View className="my-5">
-              <View className="flex flex-row items-center justify-between">
-                <Text className="text-xl font-rubik-bold text-black-300">
-                  Features
-                </Text>
-                <TouchableOpacity>
-                  <Text className="text-base font-rubik-bold text-primary-300">
-                    See all
-                  </Text>
-                </TouchableOpacity>
-              </View>
-
-              {latestPropertiesLoading ? (
-                <ActivityIndicator size="large" className="text-primary-300" />
-              ) : !latestProperties || latestProperties.length === 0 ? (
-                <NoResults />
-              ) : (
-                <FlatList
-                  data={latestProperties}
-                  renderItem={({ item }) => (
-                    <FeaturedCard
-                      item={item}
-                      onPress={() => handleCardPress(item.$id)}
-                    />
-                  )}
-                  keyExtractor={(item) => item.$id}
-                  horizontal
-                  bounces={false}
-                  showsHorizontalScrollIndicator={false}
-                  contentContainerClassName="flex gap-5 mt-5"
-                />
-              )}
-            </View>
-
-            <View className="flex flex-row items-center justify-between">
-              <Text className="text-xl font-rubik-bold text-black-300">
-                Our recommandations
+    <SafeAreaView className="bg-white h-full px-5">
+      <View className="">
+        <View className="bg-primary-100 mt-5 rounded-xl p-5">
+          <View className="flex flex-row">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-12 rounded-full"
+            />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good morning !
               </Text>
-              <TouchableOpacity>
-                <Text className="text-base font-rubik-bold text-primary-300">
-                  See all
-                </Text>
-              </TouchableOpacity>
+              <Text className="text-base font-rubik-medium text-black-300">
+                {user?.name}
+              </Text>
             </View>
-
-            <Filters />
           </View>
-        }
+
+          <View className="mt-5 flex flex-row gap-5">
+            <TouchableOpacity className="flex-1 flex items-center justify-center p-2 bg-primary-300 rounded-full">
+              <Text className="font-rubik-bold text-lg text-white">
+                Add session
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-1 flex items-center justify-center p-2 bg-primary-300 rounded-full">
+              <Text className="font-rubik-bold text-lg text-white">
+                See profiles
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+      <View className="flex flex-row items-center justify-between mt-5">
+        <Text className="text-xl font-rubik-bold text-black-300">
+          Last sessions
+        </Text>
+        <TouchableOpacity>
+          <Text className="text-base font-rubik-bold text-primary-300">
+            See all
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={latestProperties}
+        renderItem={({ item }) => (
+          <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        horizontal
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        contentContainerClassName="flex gap-5 mt-5"
       />
+
+      <View className="mb-24 flex flex-row justify-end gap-5">
+        <TouchableOpacity className="w-1/2 flex items-center justify-center p-2 bg-primary-300 rounded-full">
+          <Text className="font-rubik-bold text-lg text-white">
+            Add session
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
